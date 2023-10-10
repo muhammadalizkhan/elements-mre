4e4c6e838442de38ac24d7ec16089777a8b5152f
Michael Mersiades <fakename014@gmail.com>
2023-10-10 21:37:49 +1100

    Adds rn-elements

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,8 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import {createTheme, ThemeProvider} from '@rneui/themed';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -43,10 +42,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+  const theme = createTheme({ foo: 'bar'})
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider theme={theme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
